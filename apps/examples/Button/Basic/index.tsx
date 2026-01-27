eb76fbfd767f1391602fa78228aaf6872119acb9
Dugyu <23237626+Dugyu@users.noreply.github.com>
2026-01-27 21:46:40 +0800

    feat(luna): refine Button Basic example

--- apps/examples/Button/Basic/index.tsx
+++ apps/examples/Button/Basic/index.tsx
@@ -7,20 +7,34 @@ import { root } from '@lynx-js/react'
 import { Button } from '@lynx-js/lynx-ui-button'
 import { clsx } from 'clsx'
 
+import { Heart } from '../shared/Heart'
+
 import './index.css'
 
 function App() {
   return (
-    <view className='container lunaris-dark'>
-      <Button onClick={() => console.info('clicked')} className='button-root'>
-        {({ active = false }) => (
-          <view className={clsx('button', { 'active': active })}>
-            <text className='text'>
-              Click Me!
-            </text>
-          </view>
-        )}
-      </Button>
+    <view className='container lunaris-dark luna-gradient-rose'>
+      <view className='canvas'>
+        <Button onClick={() => console.info('clicked')} className='button-root'>
+          {({ active = false }) => (
+            <view className={clsx('button', { 'active': active })}>
+              <text className='text'>
+                Button
+              </text>
+            </view>
+          )}
+        </Button>
+        <Button className='button-root'>
+          {({ active = false }) => (
+            <view className={clsx('button', { 'active': active })}>
+              <Heart />
+              <text className='text'>
+                Button
+              </text>
+            </view>
+          )}
+        </Button>
+      </view>
     </view>
   )
 }
