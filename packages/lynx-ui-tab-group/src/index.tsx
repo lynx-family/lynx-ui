a8d3349aa4f7d74f0c68debef67be8a3fa31655e
f0rdream <14049186+f0rdream@users.noreply.github.com>
2026-08-20 14:55:05 +0800

    feat: add TabGroup primitives

---
+++ packages/lynx-ui-tab-group/src/index.tsx
@@ -0,0 +1,17 @@
+// Copyright 2026 The Lynx Authors. All rights reserved.
+// Licensed under the Apache License Version 2.0 that can be found in the
+// LICENSE file in the root directory of this source tree.
+export { TabsRoot } from './TabsRoot'
+export { TabsBar } from './TabsBar'
+export { TabsItem } from './TabsItem'
+export { TabsIndicator } from './TabsIndicator'
+
+export type {
+  TabItemProps,
+  TabsData,
+  TabsIndicatorAnimation,
+  TabsIndicatorProps,
+  TabsRootProps,
+  TabsBarProps,
+  TabsRootRef,
+} from './types'
--- tools/typedoc/index.ts
+++ tools/typedoc/index.ts
@@ -79,6 +79,13 @@ const primitivesConfig: Record<string, string[]> = {
     'InputOTP',
     'InputOTPSlot',
   ],
+  'lynx-ui-tab-group': [
+    'TabsRoot',
+    'TabsBar',
+    'TabsItem',
+    'TabItem',
+    'TabsIndicator',
+  ],
 }
 export async function runTypeDocForPackage(
   entryPoints: string | string[],
